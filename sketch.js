c9c3dcbec420500f33bfe9f0c17eea04306306d1
DamianvandeVen <contact@damianvandeven.nl>
2018-01-25 18:02:23 +0100

    ez

--- sketch.js
+++ sketch.js
@@ -1,62 +1,97 @@
-var symbolSize = 60;
-var stream;
+var streams = [];
+var fadeInterval = 1.6;
+var symbolSize = 14;
 
 function setup() {
-  createCanvas (
+  createCanvas(
     window.innerWidth,
     window.innerHeight
   );
   background(0);
-  stream = new stream();
-  stream.generateSymbols();
+
+  var x = 0;
+  for (var i = 0; i <= width / symbolSize; i++) {
+    var stream = new Stream();
+    stream.generateSymbols(x, random(-2000, 0));
+    streams.push(stream);
+    x += symbolSize
+  }
+
+  textFont('Consolas');
   textSize(symbolSize);
 }
 
 function draw() {
-  background(0);
-  steam.render();
+  background(0, 150);
+  streams.forEach(function(stream) {
+    stream.render();
+  });
 }
 
-function symbol(x, y, speed) {
+function Symbol(x, y, speed, first, opacity) {
   this.x = x;
   this.y = y;
   this.value;
+
   this.speed = speed;
-  this.switchInterval = round(random(2, 20));
+  this.first = first;
+  this.opacity = opacity;
+
+  this.switchInterval = round(random(2, 25));
 
   this.setToRandomSymbol = function() {
+    var charType = round(random(0, 5));
     if (frameCount % this.switchInterval == 0) {
-      this.value = String.fromCharCode(
-        0x30A0 + round(random(0, 96))
-      );
+      if (charType > 1) {
+        // set it to Katakana
+        this.value = String.fromCharCode(
+          0x30A0 + round(random(0, 96))
+        );
+      } else {
+        // set it to numeric
+        this.value = round(random(0,9));
+      }
     }
-
   }
 
   this.rain = function() {
     this.y = (this.y >= height) ? 0 : this.y += this.speed;
   }
+
 }
-function stream() {
-  this.symbolSize = [];
-  this.totalSymbols = round(random(5, 30));
-  this.speed = random(5, 20);
 
-  this.generateSymbols = function() {
-    var y = 0;
-    var x = width/2;
+function Stream() {
+  this.symbols = [];
+  this.totalSymbols = round(random(5, 35));
+  this.speed = random(5, 22);
 
-    for (var i = 0; i <= this.totalSymbols; i++)
-      symbol = new symbol(x, y, this.speed);
+  this.generateSymbols = function(x, y) {
+    var opacity = 255;
+    var first = round(random(0, 4)) == 1;
+    for (var i =0; i <= this.totalSymbols; i++) {
+      symbol = new Symbol(
+        x,
+        y,
+        this.speed,
+        first,
+        opacity
+      );
       symbol.setToRandomSymbol();
       this.symbols.push(symbol);
-      y -= symbolSize
-
+      opacity -= (255 / this.totalSymbols) / fadeInterval;
+      y -= symbolSize;
+      first = false;
+    }
   }
+
   this.render = function() {
     this.symbols.forEach(function(symbol) {
-      fill(0, 255, 70);
-      text(symbol.value, symbol.x, symbole.y);
+      if (symbol.first) {
+        fill(140, 255, 170, symbol.opacity);
+      } else {
+        fill(0, 255, 70, symbol.opacity);
+      }
+      text(symbol.value, symbol.x, symbol.y);
       symbol.rain();
       symbol.setToRandomSymbol();
     });
